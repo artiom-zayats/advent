990c1a6afcb1845d4ea6321a2f35896782e0ea73
Tom Zayats <tzayats@zulily.com>
2020-12-04 09:16:27 -0800

    added problem 1 3sum'

--- problem_1.py
+++ problem_1.py
@@ -5,21 +5,34 @@ def load(filename):
     content = [int(x.strip()) for x in content]
     return content
 
-def find2sum(numbers):
+def find2sum(numbers,target):
     d = {}
     for n in numbers:
         if n in d:
             return n,d[n]
         else:
-            d[2020-n]=n
+            d[target-n]=n
+    return None
+
+def find3sum(numbers,target):
+
+    numbers = sorted(numbers)
+    for i in range(len(numbers)-2):
+        ans = find2sum(numbers[i:],target - numbers[i])
+        if ans:
+            return numbers[i],ans[0],ans[1]
+
+
+    return None
 
 
 
 def main():
     x = load("inputs/input_1.txt")
     print(x)
-    a,b = find2sum(x)
-    print(a,b,a+b,a*b)
+
+    a,b,c = find3sum(x,2020)
+    print(a,b,c,a+b+c,a*b*c)
 
 if __name__ == '__main__':
     main()
